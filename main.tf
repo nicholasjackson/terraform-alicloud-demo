e0aa7073cdec57f1cafd40a61436267df9034a11
Nic Jackson <jackson.nic@gmail.com>
2018-06-30 10:11:51 +0100

    Added module

--- main.tf
+++ main.tf
@@ -48,3 +48,37 @@ resource "alicloud_instance" "web" {
   instance_name        = "${terraform.workspace}-web"
   vswitch_id           = "${alicloud_vswitch.vsw.id}"
 }
+
+module "webapp" {
+  source = "github.com/nicholasjackson/alicloud-terraform-templates/hosting/web-application-hosting"
+
+  region        = "eu-central-1"
+  solution_name = "test"
+
+  web_layer_name         = "weblayer"
+  web_instance_count     = "1"
+  web_availability_zone  = "a"
+  web_instance_type      = "${data.alicloud_instance_types.2c4g.instance_types.0.id}"
+  web_instance_port      = "80"
+  web_instance_image_id  = "ubuntu_16_0402_64_20G_alibase_20170818.vhd"
+  web_instance_user_data = ""
+
+  app_layer_name         = "applayer"
+  app_instance_count     = "1"
+  app_availability_zone  = "a"
+  app_instance_type      = "${data.alicloud_instance_types.2c4g.instance_types.0.id}"
+  app_instance_port      = "5000"
+  app_instance_image_id  = "ubuntu_16_0402_64_20G_alibase_20170818.vhd"
+  app_instance_user_data = ""
+
+  db_layer_name        = "dblayer"
+  db_availability_zone = "a"
+  db_engine            = "MySQL"
+  db_engine_version    = "5.6"
+  db_instance_type     = "mysql.n1.micro.1"
+  db_instance_storage  = "20"
+}
+
+output "slb_web_public_ip" {
+  value = "${module.webapp.slb_web_public_ip}"
+}
