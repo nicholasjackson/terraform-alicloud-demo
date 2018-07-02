b4735bab0e623278f78f33a5b0bff7278e20964a
Nic Jackson <jackson.nic@gmail.com>
2018-07-02 14:40:39 +0200

    Awesome changes

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
 provider "alicloud" {}
 
+terraform {
+  backend "s3" {
+    bucket   = "tfstate"
+    key      = "simpledemo/terraform.tfstate"
+    region   = "us-east-1"
+    endpoint = "https://oss-eu-central-1.aliyuncs.com"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms      = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+  }
+}
+
 data "alicloud_instance_types" "2c4g" {
   cpu_core_count = 1
   memory_size    = 1
