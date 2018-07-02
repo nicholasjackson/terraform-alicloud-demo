9661be89eaa58e47bbe50b514064bfd39ff39e96
Nic Jackson <jackson.nic@gmail.com>
2018-07-02 12:35:02 +0200

    Basic config

--- main.tf
+++ main.tf
@@ -1,17 +1,4 @@
-terraform {
-  backend "s3" {
-    bucket   = "tfstate"
-    key      = "demo/terraform.tfstate"
-    region   = "us-east-1"
-    endpoint = "https://oss-eu-central-1.aliyuncs.com"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_get_ec2_platforms      = true
-    skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
-  }
-}
+provider "alicloud" {}
 
 data "alicloud_instance_types" "2c4g" {
   cpu_core_count = 1
@@ -47,38 +34,4 @@ resource "alicloud_instance" "web" {
   security_groups      = ["${alicloud_security_group.default.id}"]
   instance_name        = "${terraform.workspace}-web"
   vswitch_id           = "${alicloud_vswitch.vsw.id}"
-}
-
-module "webapp" {
-  source = "github.com/nicholasjackson/alicloud-terraform-templates/hosting/web-application-hosting"
-
-  region        = "eu-central-1"
-  solution_name = "test"
-
-  web_layer_name         = "weblayer"
-  web_instance_count     = "1"
-  web_availability_zone  = "a"
-  web_instance_type      = "${data.alicloud_instance_types.2c4g.instance_types.0.id}"
-  web_instance_port      = "80"
-  web_instance_image_id  = "ubuntu_16_0402_64_20G_alibase_20170818.vhd"
-  web_instance_user_data = ""
-
-  app_layer_name         = "applayer"
-  app_instance_count     = "1"
-  app_availability_zone  = "a"
-  app_instance_type      = "${data.alicloud_instance_types.2c4g.instance_types.0.id}"
-  app_instance_port      = "5000"
-  app_instance_image_id  = "ubuntu_16_0402_64_20G_alibase_20170818.vhd"
-  app_instance_user_data = ""
-
-  db_layer_name        = "dblayer"
-  db_availability_zone = "a"
-  db_engine            = "MySQL"
-  db_engine_version    = "5.6"
-  db_instance_type     = "mysql.n1.micro.1"
-  db_instance_storage  = "20"
-}
-
-output "slb_web_public_ip" {
-  value = "${module.webapp.slb_web_public_ip}"
 }
